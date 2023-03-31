22f3e23532bed042d0eac9cc0eef079293b1fd5e
HyunwooKim01 <126743699+HyunwooKim01@users.noreply.github.com>
2023-03-31 16:33:38 +0900

    Add files via upload

---
+++ YS-C++/23.03.31-C++.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Line {
+public:
+	Line();
+	void setTwoPoints();
+	double getLineLength();
+	int sx = 0, sy = 1;
+	int ex = 0, ey = 1;
+};
+Line::Line() {}
+void Line::setTwoPoints() { 
+	cout << "시작점 좌표 두개를 입력하시오.: ";
+	cin >> sx >> sy;
+	cout << "끝점 좌표 두개를 입력하시오.: ";
+	cin >> ex >> ey;
+}
+double Line::getLineLength() {
+		return sqrt(pow(ex - sx, 2) + pow(ey - sy, 2));
+}
+
+int main()
+{
+	Line myline;
+	myline.setTwoPoints();
+	cout << "myline의길이는" << myline.getLineLength() << " 입니다. \n";
+	
+}
